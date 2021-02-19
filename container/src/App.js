32eb9f4e082f7a32f29463413d9964f3be6f0faf
mahaldi <aldiwijayasaputra@gmail.com>
2021-02-19 15:21:42 +0700

    Update App.js

--- container/src/App.js
+++ container/src/App.js
@@ -3,6 +3,7 @@ import MarketingApp from './components/MarketingApp'
 console.log('testing yaml action 4')
 export default () => {
     return <div>
+        <h1>Container Here!</h1>
         <MarketingApp />
     </div>
 }
