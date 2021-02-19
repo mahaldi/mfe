7de7b2941ee6594e0ebef45b9949ca2f696a21aa
mahaldi <aldiwijayasaputra@gmail.com>
2021-02-19 14:53:10 +0700

    Update App.js

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import MarketingApp from './components/MarketingApp'
-console.log('testing yaml action 2')
+console.log('testing yaml action 3')
 export default () => {
     return <div>
         <MarketingApp />
