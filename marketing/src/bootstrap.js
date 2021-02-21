5212451a5f276fca07111e8e4f872d7c700c93f2
mahaldi <aldiwijayasaputra@gmail.com>
2021-02-21 18:42:00 +0700

    Update bootstrap.js

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App'
+import {createMemoryHistory, createBrowserHistory} from 'history'
 
-const mount = (el) => {
+const mount = (el, { onNavigation, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory()
+    if(onNavigation) history.listen(onNavigation)
     ReactDom.render(
-        <App />,
+        <App history={history}/>,
         el
     )
+    return {
+        onParentNavigation: ({pathname: nextPathname}) => {
+            const {pathname} = history.location
+            
+            if(pathname !== nextPathname) {
+                history.push(nextPathname)
+            }
+        }
+    }
 }
 
 if(process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root')
 
     if(devRoot) {
-        mount(devRoot)
+        mount(devRoot, { defaultHistory: createBrowserHistory() })
     }
 }
 
