be9c50b96dccc178594c8219c3b982981acbf571
Mahaldi Wijaya Saputra <mahaldi.wijaya@dansmultipro.com>
2023-10-13 17:04:49 +0700

    first attempt pwa config test

--- container/config/webpack.common.js
+++ container/config/webpack.common.js
@@ -1,4 +1,7 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const path = require('path')
+const assetPath = './static'
 
 module.exports = {
     module: {
@@ -18,7 +21,24 @@ module.exports = {
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: './public/index.html'
+            template: './public/index.html',
+            filename: './index.html',
+            favicon: './public/favicon.ico',
+            manifest: './public/manifest.json' // not working
+        }),
+        new WebpackPwaManifest({
+            publicPath: '/',
+            name: 'My Progressive Web App',
+            short_name: 'MyPWA',
+            description: 'My awesome Progressive Web App!',
+            background_color: '#ffffff',
+            // crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+            icons: [
+              {
+                src: path.resolve(`${assetPath}/logo512.png`),
+                size: '512x512' // you can also use the specifications pattern
+              }
+            ]
         })
     ]
 }
