cc4385ea3d87ceb0b69856c76fef5536eab522a8
Mahaldi Wijaya Saputra <mahaldi.wijaya@dansmultipro.com>
2023-10-18 00:58:32 +0700

    testing add service worker

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App'
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 ReactDom.render(
     <App />,
     document.querySelector('#root')
-)
+);
+serviceWorkerRegistration.register();
