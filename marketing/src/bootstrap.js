e7a996c9907791d1ee78ffe24d8f4d171a0c1848
Mahaldi Wijaya Saputra <mahaldi.wijaya@dansmultipro.com>
2023-10-18 03:08:41 +0700

    Update bootstrap.js

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -9,7 +9,6 @@ const mount = (el, { onParentNavigation, defaultHistory, initialPath }) => {
     })
     if(onParentNavigation) history.listen(onParentNavigation)
     console.log('before render on mount marketing', App)
-
     ReactDom.render(
         <App history={history}/>,
         el
